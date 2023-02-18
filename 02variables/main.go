c50a4010762a62965c2a4c87adc8fd23ccbc667d
SubhamATRhoneysys <subham.d@honeysys.com>
2023-02-19 01:19:15 +0530

    day 1

---
+++ 02variables/main.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+func main(){
+	// fmt.Println("subha,")
+	var username string="subham"
+	fmt.Println(username)
+	fmt.Printf("variable is:%T \n",username)
+
+	var isLoggedIn bool=false
+	fmt.Println(isLoggedIn)
+	fmt.Printf("variabe %T \n", isLoggedIn)
+
+    var anotherVariable int
+	fmt.Println(anotherVariable)
+	fmt.Printf("dsf")
+}
