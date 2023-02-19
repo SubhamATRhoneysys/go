0b5c167fe3418bd3a0d92a52543a7da332fa9dd2
SubhamATRhoneysys <subham.d@honeysys.com>
2023-02-19 23:06:41 +0530

    day 2

---
+++ 11mystructs/main.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+func main(){
+	fmt.Println()
+	// no inheritance in golang no super or parent
+
+}
+type User struct{
+	Name string
+	Email string
+	States bool
+	Age int
+}
