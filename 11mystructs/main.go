cb2306f823166768d1dcb28acede42d40e02376e
SubhamATRhoneysys <subham.d@honeysys.com>
2023-02-24 10:33:48 +0530

    commit

--- 11mystructs/main.go
+++ 11mystructs/main.go
@@ -1,13 +1,22 @@
 package main
+
 import "fmt"
-func main(){
+
+func main() {
 	fmt.Println()
 	// no inheritance in golang no super or parent
+	subham := User{"subham", "deysubham999@gmail.com", true, 23}
+	fmt.Println(subham)
+	fmt.Printf("hitesh details are: %v\n",subham)
+	fmt.Printf("hitesh details are: %v\n",subham.Name)
+	fmt.Printf("hitesh details are: %v\n",subham.Age)
+
 
 }
-type User struct{
-	Name string
-	Email string
+
+type User struct {
+	Name   string
+	Email  string
 	States bool
-	Age int
+	Age    int
 }
